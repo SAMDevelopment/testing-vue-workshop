a85814d20473ca85fb84d7f5918fc4c4c2d70a83
Sam de Vries <sam@myparcel.com>
2018-09-20 15:07:02 +0100

    :white_check_mark: Finished async-code exercise tests.

--- exercises/6-async-code/ItemView.spec.js
+++ exercises/6-async-code/ItemView.spec.js
@@ -1,20 +1,27 @@
 import { fetchItems } from './api'
 import ItemView from './ItemView.vue'
+import Item from './Item.vue'
 import { shallowMount } from '@vue/test-utils'
 import flushPromises from 'flush-promises'
 
 jest.mock('./api')
 
 test('renders items resolved from fetchItems', async () => {
-  // expect.assertions(2)
+  expect.assertions(1)
   fetchItems.mockResolvedValue(['item-1', 'item-2'])
   const wrapper = shallowMount(ItemView)
 
   await flushPromises()
 
-
+  expect(wrapper.findAll(Item)).toHaveLength(2)
 })
 
-test('renders error if fetchItems rejects', () => {
+test('renders error if fetchItems rejects', async () => {
+  expect.assertions(1)
+  fetchItems.mockRejectedValue(new Error('No items!'))
+  const wrapper = shallowMount(ItemView)
 
+  await flushPromises()
+
+  expect(wrapper.text()).toContain('Error loading items')
 })
