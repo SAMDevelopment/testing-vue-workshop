1452d3c35b03edbf8cb1d1a94ae93d68b15c61f8
Sam de Vries <sam@myparcel.com>
2018-09-20 15:03:20 +0100

    :recycle: :white_check_mark: Improved timer-functions exercise tests.

--- exercises/7-timer-functions/ProgressBar.spec.js
+++ exercises/7-timer-functions/ProgressBar.spec.js
@@ -6,11 +6,14 @@ jest.useFakeTimers()
 test('increments width by 1% every 100ms', () => {
   const wrapper = shallowMount(ProgressBar)
 
-  expect(wrapper.attributes('style')).toBe('width: 0%;')
+  console.log(
+
+  )
+  expect(wrapper.element.style.width).toBe('0%;')
 
   jest.advanceTimersByTime(300)
-  expect(wrapper.attributes('style')).toBe('width: 3%;')
+  expect(wrapper.element.style.width).toBe('3%;')
 
   jest.advanceTimersByTime(100)
-  expect(wrapper.attributes('style')).toBe('width: 4%;')
+  expect(wrapper.element.style.width).toBe('4%;')
 })
