dc44b7813176cb2054e38f9c2326c3584f1303f7
Sam de Vries <sam@myparcel.com>
2018-09-20 12:24:31 +0100

    :white_check_mark: Added stubbing-components exercise tests.

--- exercises/4-stubbing-components/Menu.spec.js
+++ exercises/4-stubbing-components/Menu.spec.js
@@ -1,3 +1,31 @@
-test('renders router-link to /login page when signedIn is false')
+import Menu from './Menu.vue'
+import { RouterLinkStub, shallowMount } from '@vue/test-utils'
 
-test('renders <a> that signs user out if signedIn is true')
+test('renders router-link to /login page when signedIn is false', () => {
+  const wrapper = shallowMount(Menu, {
+    propsData: {
+      signedIn: true
+    },
+    stubs: {
+      'router-link': RouterLinkStub
+    }
+  })
+
+  expect(wrapper.text()).toContain('Sign in')
+  expect(wrapper.text()).not.toContain('Sign out')
+  expect(wrapper.find(RouterLinkStub).props('to')).toBe('/login')
+})
+
+test('renders <a> that signs user out if signedIn is true', () => {
+  const wrapper = shallowMount(Menu, {
+    propsData: {
+      signedIn: true
+    },
+    stubs: {
+      'router-link': RouterLinkStub
+    }
+  })
+
+  expect(wrapper.text()).toContain('Sign out')
+  expect(wrapper.text()).not.toContain('Sign in')
+})
