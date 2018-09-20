2fac735c8e138748bc5fcb2ab84298b86e7c299e
Sam de Vries <sam@myparcel.com>
2018-09-20 11:58:32 +0100

    :white_check_mark: Added vue-events exercise tests.

--- exercises/3-vue-events/App.spec.js
+++ exercises/3-vue-events/App.spec.js
@@ -1,1 +1,11 @@
-test('removes Modal when close-modal is emitted')
+import App from './App.vue'
+import Modal from './Modal.vue'
+import { shallowMount } from '@vue/test-utils'
+
+test('removes Modal when close-modal is emitted', () => {
+  const wrapper = shallowMount(App)
+
+  expect(wrapper.find(Modal).exists()).toBe(true)
+  wrapper.find(Modal).vm.$emit('close-modal')
+  expect(wrapper.find(Modal).exists()).toBe(false)
+})
--- exercises/3-vue-events/Modal.spec.js
+++ exercises/3-vue-events/Modal.spec.js
@@ -1,1 +1,10 @@
-test('emits on-close event when close button is clicked')
+import Modal from './Modal.vue'
+import { shallowMount } from '@vue/test-utils'
+
+test('emits on-close event when close button is clicked', () => {
+  const wrapper = shallowMount(Modal)
+
+  expect(wrapper.emitted('close-modal')).toBe(undefined)
+  wrapper.find('button').trigger('click')
+  expect(wrapper.emitted('close-modal')).toEqual([[]])
+})
