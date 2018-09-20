3524f8563b0d61e72a1adf3d5c7150c10aeb98b8
Sam de Vries <sam@myparcel.com>
2018-09-20 15:02:10 +0100

    :white_check_mark: Added timer-functions exercise tests.

--- exercises/7-timer-functions/ProgressBar.spec.js
+++ exercises/7-timer-functions/ProgressBar.spec.js
@@ -1,1 +1,16 @@
-test('increments width by 1% every 100ms')
+import ProgressBar from './ProgressBar.vue'
+import { shallowMount } from '@vue/test-utils'
+
+jest.useFakeTimers()
+
+test('increments width by 1% every 100ms', () => {
+  const wrapper = shallowMount(ProgressBar)
+
+  expect(wrapper.attributes('style')).toBe('width: 0%;')
+
+  jest.advanceTimersByTime(300)
+  expect(wrapper.attributes('style')).toBe('width: 3%;')
+
+  jest.advanceTimersByTime(100)
+  expect(wrapper.attributes('style')).toBe('width: 4%;')
+})
