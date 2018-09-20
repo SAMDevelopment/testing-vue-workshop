90830ad172ea5b064e5b0f7aae46935c07be9180
Sam de Vries <sam@myparcel.com>
2018-09-20 13:08:34 +0100

    :white_check_mark: Added mocks exercise tests.

--- exercises/5-mocks/Modal.spec.js
+++ exercises/5-mocks/Modal.spec.js
@@ -2,4 +2,15 @@ import { shallowMount } from '@vue/test-utils'
 import Modal from './Modal.vue'
 
 // Use jest.fn to create a mock function
-test('calls onClose when close button is clicked')
+test('calls onClose when close button is clicked', () => {
+  const onCloseMock = jest.fn()
+  const wrapper = shallowMount(Modal, {
+    propsData: {
+      onClose: onCloseMock
+    }
+  })
+
+  wrapper.find('button').trigger('click')
+
+  expect(onCloseMock).toHaveBeenCalled()
+})
--- exercises/5-mocks/SubmitForm.spec.js
+++ exercises/5-mocks/SubmitForm.spec.js
@@ -1,4 +1,17 @@
 import { shallowMount } from '@vue/test-utils'
 import SubmitForm from './SubmitForm.vue'
 
-test('calls onSubmit with value of input when form is submitted')
+test('calls onSubmit with value of input when form is submitted', () => {
+  const onSubmitMock = jest.fn()
+  const wrapper = shallowMount(SubmitForm, {
+    propsData: {
+      onSubmit: onSubmitMock
+    }
+  })
+
+  // Prefer this over mutating the data directly to make sure it uses the value from the input.
+  wrapper.find('[type="text"]').setValue('my-username')
+  wrapper.find('form').trigger('submit')
+
+  expect(onSubmitMock).toHaveBeenCalledWith('my-username')
+})
