f8319b470514365faa16202a4e6ac31b89da03ad
Sam de Vries <sam@myparcel.com>
2018-09-20 14:51:45 +0100

    :construction: :white_check_mark: Working on async-code exercise tests.

--- exercises/6-async-code/ItemView.spec.js
+++ exercises/6-async-code/ItemView.spec.js
@@ -1,5 +1,20 @@
-import ItemView from './ItemView'
+import { fetchItems } from './api'
+import ItemView from './ItemView.vue'
+import { shallowMount } from '@vue/test-utils'
+import flushPromises from 'flush-promises'
 
-test('renders items resolved from fetchItems')
+jest.mock('./api')
 
-test('renders error if fetchItems rejects')
+test('renders items resolved from fetchItems', async () => {
+  // expect.assertions(2)
+  fetchItems.mockResolvedValue(['item-1', 'item-2'])
+  const wrapper = shallowMount(ItemView)
+
+  await flushPromises()
+
+
+})
+
+test('renders error if fetchItems rejects', () => {
+
+})
--- exercises/6-async-code/api.spec.js
+++ exercises/6-async-code/api.spec.js
@@ -1,3 +1,18 @@
-test('resolves with data.items if /items returns 200')
+import { fetchItems } from './api'
+import axios from 'axios'
 
-test('rejects with error if /items returns 500')
+jest.mock('axios')
+
+test('resolves with data.items if /items returns 200', async () => {
+  expect.assertions(1)
+  axios.get.mockResolvedValue(['item-1', 'item-2'])
+
+  const data = await fetchItems()
+  expect(data).toHaveLength(2)
+})
+
+test('rejects with error if /items returns 500', () => {
+  axios.get.mockRejectedValue(new Error('500'))
+
+  expect(fetchItems()).rejects.toThrow('500')
+})
