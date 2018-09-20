9a85f095a8fbd7d6e5927f2b109d62cf11adef70
Sam de Vries <sam@myparcel.com>
2018-09-20 10:39:18 +0200

    :recycle: :whale: Renamed web service to node.

--- sam.sh
+++ sam.sh
@@ -4,9 +4,9 @@ set -eo pipefail
 if [ $# -gt 0 ]; then
 
   # Start services.
-  if [ "$1" == "web" ]; then
+  if [ "$1" == "node" ]; then
     shift 1
-    docker-compose run --rm web "$@"
+    docker-compose run --rm node "$@"
   else
     docker-compose "$@"
   fi
